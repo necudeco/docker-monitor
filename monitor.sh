59f5af60f3bdd203a186acd4b6d0452a0c89a054
Renato Amaya <necudeco@gmail.com>
2022-09-09 07:15:56 -0500

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "Monitor Docker Events"
+
+source ./.monitor.ini
+
+domain=$MONITOR_DOMAIN
+from=$MONITOR_FROM
+to=$MONITOR_TO
+api_key=$MONITOR_APIKEY
+
+URL="https://api.mailgun.net/v3/$domain/messages"
+server_name=$(hostname -f )
+
+function sendMail(){
+        container_name=$1
+        echo ""
+        timestamp=$(date +%s)
+        log=$timestamp
+        #log=$(docker logs --tail 50 ${container_name})
+        docker logs --tail 50 ${container_name} &> "${timestamp}.log"
+
+        req="curl --user 'api:${api_key}' '$URL' -F from='${from}' -F to='${to}'" + \
+                "  -F subject='Container started ${container_name} on ${server_name}' "+ \
+                "  -F text='${log}' -F attachment='@${timestamp}.log' "
+
+        #echo "${req}"
+        eval $req
+        rm "${timestamp}.log"
+}
+
+
+docker events --filter 'type=container' --filter 'event=start' --format '{{.Actor.Attributes.name}}' |
+while read container; do
+      echo " container started ${line}" ;
+      sendMail $container
+done
